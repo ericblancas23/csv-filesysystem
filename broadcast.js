49a2cb40a31bf9ad4e6161128c7ed5487369cf6e
EricB <ericblancas23@gmail.com>
2018-12-03 22:50:46 -0800

    added arrays

--- broadcast.js
+++ broadcast.js
@@ -2,11 +2,33 @@ const program = require('commander');
 const csv = require('csv');
 const fs = require('fs');
 
+
+
 program 
     .version('0.0.1')
     .option('-l, --list, [list]', 'list of customers in csv file')
-    .parse(process.argv)
+    .parse(process.argv);
 
+let questions = [
+    {
+        type: "input",
+        name: "sender.name",
+        message: "Sender's email address"
+    },
+    {
+        type: "input",
+        name: "sender.name",
+        message: "Sender's name"
+    },
+    {
+        type: "input",
+        name: "subject",
+        message: "Subject - "
+    }
+]
+
+
+let  contactList = []
 let parse = csv.parse;
 let stream = fs.createReadStream(program.list)
     .pipe(parse({ delimiter: ','}));
