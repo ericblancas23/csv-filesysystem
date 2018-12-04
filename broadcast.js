4f32daf7a80c18b0240ad9686b577d4fb80e331b
EricB <ericblancas23@gmail.com>
2018-12-04 13:23:07 -0800

    added new asyn methods

--- broadcast.js
+++ broadcast.js
@@ -33,6 +33,27 @@ let parse = csv.parse;
 let stream = fs.createReadStream(program.list)
     .pipe(parse({ delimiter: ','}));
 
+
+let __sendEmail = function(to, from, subject, callback) {
+    let template = "Wishing you a Merry Christmas and a " + 
+        "prosperous year ahead. P.S Toby I hate you";
+    let helper = require('sendgrid').mail;
+    let fromEmail = new helper.Email(from.email, from.name);
+    let toEmail = new helper.Email(to.email, to.name);
+    let body = new helper.Content("tex/plain", template);
+    let mail = new helper.Mail(fromEmail, subject,toEmail, body);
+
+    let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
+    let request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON()
+
+    })
+
+}
+
 stream
     .on('error', function(error) {
         return console.error(err.message);
